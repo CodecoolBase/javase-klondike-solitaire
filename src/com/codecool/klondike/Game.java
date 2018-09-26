c3a882b5fe3045602b9fbc911a2096ef0e9ab2e1
Jarosław Wałach <jaroslaw.walach@gmail.com>
2018-09-26 11:43:14 +0200

    Implement refill of stock from discard

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -13,9 +13,7 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Game extends Pane {
 
@@ -106,7 +104,11 @@ public class Game extends Pane {
     }
 
     public void refillStockFromDiscard() {
-        //TODO
+        Collections.reverse(discardPile.getCards());
+        discardPile.getCards().forEach(card -> card.flip());
+
+        MouseUtil.slideToDest(discardPile.getCards(), stockPile);
+
         System.out.println("Stock refilled from discard pile.");
     }
 
